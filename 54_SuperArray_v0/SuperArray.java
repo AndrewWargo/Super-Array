937d73f82f1ef95fa165bd4d423f35cc48d3f389
awargo@stuy.edu <awargo@stuy.edu>
2017-12-14 12:25:48 -0500

    Let's goo

---
+++ 54_SuperArray_v0/SuperArray.java
@@ -0,0 +1,44 @@
+/***************************
+ * class SuperArray
+ * Wrapper class for array. Facilitates resizing,
+ * getting and setting element values.
+ *
+ * skeleton by Mr. Brown Mykolyk
+ ***************************/
+
+public class SuperArray {
+
+    private int[] _data;  //underlying container
+    private int _size;    //number of elements in this SuperArray
+
+
+    //default constructor – initializes 10-item array
+    public SuperArray() {
+        /* YOUR IMPLEMENTATION HERE */
+    }
+
+
+    // represent SuperArray in [a,b,c] format
+    public String toString() {
+        /* YOUR IMPLEMENTATION HERE */
+    }
+
+
+    //double the capacity of SuperArray
+    private void expand() {
+        /* YOUR IMPLEMENTATION HERE */
+    }
+
+
+    //accessor -- return value at specified index
+    public int get( int index ) {
+        /* YOUR IMPLEMENTATION HERE */
+    }
+
+
+    //mutator -- set value at index to newVal,
+    //           return old value at index
+    public int set( int index, int newVal ) {
+        /* YOUR IMPLEMENTATION HERE */
+    }
+}//end class
---
+++ 54_SuperArray_v0/UserOfSuperArray.java
@@ -0,0 +1,27 @@
+/** Test SuperArray features
+ *  work by Mr. Brown Mykolyk, used with thanks
+ *  
+ */
+
+public class UserOfSuperArray {
+
+    public static void main( String[] args ) {
+        SuperArray curtis = new SuperArray();
+        // System.out.println( "Printing empty SuperArray curtis..." );
+        // System.out.println( curtis );
+
+        // // populate the sample array
+        // for( int i = 0; i < curtis._data.length; i++ ) {
+            // curtis.set( i, i * 2 );
+        // }
+        // System.out.println("Printing populated SuperArray curtis...");
+        // System.out.println(curtis);
+
+        // // test expansion
+        // for( int i = 0; i < 3; i++ ) {
+            // curtis.expand();
+            // System.out.println("Printing expanded SuperArray curtis...");
+            // System.out.println(curtis);
+        // }
+    }//end main()
+}
