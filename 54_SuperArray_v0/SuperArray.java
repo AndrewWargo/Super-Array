ad7f6f48f885a78c1d770c16b7dd92d25afa54e4
AndrewWargo <awargo@stuy.edu>
2017-12-17 17:59:35 -0500

    Homework completed

--- 54_SuperArray_v0/SuperArray.java
+++ 54_SuperArray_v0/SuperArray.java
@@ -8,55 +8,93 @@
 
 public class SuperArray {
 
-    private int[] _data;  //underlying container
-    private int _size;    //number of elements in this SuperArray
+	private int[] _data;  // underlying container
+    private int _size;    // the number of elements in the list
+    private int lastIndex;
 
-
-    //default constructor – initializes 10-item array
+    /** 
+      default constructor
+      Constructs an empty list with an initial capacity of ten.
+     */
     public SuperArray() {
         _data = new int[10];
         _size = _data.length;
-    }
-
-    //returns length of _data
-    public int size() {
-    	return _size;          
+        lastIndex = -1;
     }
 
 
-    // represents SuperArray in [a,b,c] format
-    public String toString() {
-	String x = "[";                        
-	for(int i = 0; i < _data.length; i++){  
-	    x = x + _data[i] + ",";
-	}
-	return x + "]";
-    }
+    /** 
+      @return the number of elements in this list
+     */
+     public int size() {
+     	return _size; 
+     }
 
 
-    //doubles the capacity of SuperArray
-    public void expand() {
-        int[] x = new int[_size * 2]; //Constructs a reference to a new array  with double the length of _data
-        for(int i = 0; i < _size; i++) { 
-	    x[i] = _data[i]; //Adds the values in _data to the new array, in the same position as data
-        }
-        _data = x; //Sets _data to the array referenced by x, which is itself _data with double the length
-        _size = _data.length; //Sets the size variable to the length of the new array
-    }
+     /** 
+       @return a string representation of the list, in [a,b,c,] format
+      */ 
+     public String toString() {
+    		String x = "[";                        
+    		for(int i = 0; i < _data.length; i++){  
+    		    x = x + _data[i] + ",";
+    		}
+    		return x + "]";
+     }
 
 
-    //accessor -- returns value at specified index
-    public int get( int index ) {
-        return _data[index];
-    }
+    /** 
+      Appends @value to the end of this list.
+      
+      @return true, in keeping with conventions yet to be discussed
+     */
+      public boolean add( int value) {
+    	  if(lastIndex == _size - 1) {
+    		  expand();
+    	  }
+    	  _data[lastIndex + 1] = value;
+    	  lastIndex++;
+    	  return true;
+      }
 
 
-    //mutator -- sets value at index to newVal,
-    //           return old value at index
-    public int set( int index, int newVal ) { 
-        int y = get(index); //Sets int y to the value held in _data[index] MUST BE FIRST, otherwise the function would return the new value
-        _data[index] = newVal; //Sets the value held in _data[index] to the new value
-        return y; //Returns the old value
-    }
-}//end class
+    /** 
+      Doubles the capacity of the SuperArray, 
+      preserving extant data
+     */
+      private void expand() {
+          int[] x = new int[_size * 2]; //Constructs a reference to a new array  with double the length of _data
+          for(int i = 0; i < _size; i++) { 
+  	    x[i] = _data[i]; //Adds the values in _data to the new array, in the same position as data
+          }
+          _data = x; //Sets _data to the array referenced by x, which is itself _data with double the length
+          _size = _data.length; //Sets the size variable to the length of the new array
+      }
+
+     
+    /** 
+      accessor  
+      @return element @index from the list
+      precondition: @index is within the bounds of the array.
+          (Having warned the user about this precondition,
+           you should NOT complicate your code to check
+           whether user violated the condition.)
+     */
+     public int get( int index ) {
+         return _data[index];
+     }
+
+
+    /** 
+      Set value at @index to @newValue
+      
+      @return old value at @index
+      @precondition: @index is within the bounds of the list.
+     */
+     public int set( int index, int newValue ) {
+         int y = get(index); //Sets int y to the value held in _data[index] MUST BE FIRST, otherwise the function would return the new value
+         _data[index] = newValue; //Sets the value held in _data[index] to the new value
+         return y; //Returns the old value
+     }
+}//end of class
 
